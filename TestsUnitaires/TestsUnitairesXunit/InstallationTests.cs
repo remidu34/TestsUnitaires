b0a6362050ff96dba0081edf90f53437cf4b7208
remi.pro <remi.arnaud.pro@outlook.fr>
2022-01-05 09:41:21 +0100

    Installation/Menu

--- TestsUnitaires/TestsUnitairesXunit/InstallationTests.cs
+++ TestsUnitaires/TestsUnitairesXunit/InstallationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using TestsUnitaires;
 using Xunit;
 
 namespace LeGrandRestaurant.Test
--- TestsUnitaires/TestsUnitairesXunit/MenusTests.cs
+++ TestsUnitaires/TestsUnitairesXunit/MenusTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using TestsUnitaires;
+using Xunit;
 
 namespace LeGrandRestaurant.Test
 {
