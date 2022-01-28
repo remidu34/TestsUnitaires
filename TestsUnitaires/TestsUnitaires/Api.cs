3d8716b16073679f7f916dc9b62a2a87c09c434d
unknown <mehdibarchi@icloud.com>
2022-01-28 12:00:50 +0100

    Fonctionnel

---
+++ TestsUnitaires/TestsUnitaires/Api.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TestsUnitaires
+{
+    public class Api : Controller
+    {
+        public Serveur GetServeur()
+        {
+            Serveur serveur = new Serveur();
+            return serveur;
+        }
+
+        public Serveur GetServeurCommande()
+        {
+            Serveur serveur = new Serveur();
+            serveur.PrendCommande(25);
+            return serveur;
+        }
+
+        public Serveur GetServeurRecommande()
+        {
+            Serveur serveur = new Serveur();
+            serveur.PrendCommande(25);
+            serveur.PrendCommande(75);
+            return serveur;
+        }
+    }
+}
---
+++ TestsUnitaires/TestsUnitairesXunit/ApiTests.cs
@@ -0,0 +1,53 @@
+﻿using TestsUnitaires;
+using Xunit;
+
+namespace TestsUnitairesXunit
+{
+    public class ApiTests
+    {
+        [Fact(DisplayName = "On vérifie qu'un serveur ait un CA à 0")]
+        public void TestValuesCreateServeur()
+        {
+            // Arrange
+            var client = new Api();
+
+            // Act
+            var response = client.GetServeur();
+
+            // Assert
+            var value = response;
+            Assert.NotEmpty(value.name);
+            Assert.Equal(0, value.ChiffreAffaire);
+        }
+
+        [Fact(DisplayName = "On vérifie qu'un serveur ait un CA à 25€ après une commande")]
+        public void TestValuesCommandeServeur()
+        {
+            // Arrange
+            var client = new Api();
+
+            // Act
+            var response = client.GetServeurCommande();
+
+            // Assert
+            var value = response;
+            Assert.NotEmpty(value.name);
+            Assert.Equal(25, value.ChiffreAffaire);
+        }
+
+        [Fact(DisplayName = "On vérifie qu'un serveur ait un CA à 100€ après avoir repris une commande")]
+        public void TestValuesRecommandeServeur()
+        {
+            // Arrange
+            var client = new Api();
+
+            // Act
+            var response = client.GetServeurRecommande();
+
+            // Assert
+            var value = response;
+            Assert.NotEmpty(value.name);
+            Assert.Equal(100, value.ChiffreAffaire);
+        }
+    }
+}
